0e1844b8bd50e17733bc77d2f8b5dd508fffc55c
syed hussain <syedhussain@syeds-MBP.home>
2015-11-04 01:03:57 -0500

    Added styles

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,12 @@ var myApp = angular.module('myApp', ['ui.router']);
 myApp.config(['$stateProvider','$urlRouterProvider' ,function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
+  $urlRouterProvider.otherwise("/");
   //
   // Now set up the states
   $stateProvider
     .state('state1', {
-      url: "/state1",
+      url: "/",
       templateUrl: "views/main.html"
     })
     .state('state2',{
