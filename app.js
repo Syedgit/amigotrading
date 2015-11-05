0c9dbb265a36e9f793d3e49a5b7bf8c8ebe8f74d
syed hussain <syedhussain@syeds-MBP.home>
2015-11-05 01:04:35 -0500

    Added tons of pages

--- app.js
+++ app.js
@@ -17,3 +17,30 @@ var server = app.listen(3000, function(){
 
 	console.log('Example app listening at http',host,port);
 });
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'sql1'
+});
+var app = express();
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... \n\n");  
+} else {
+    console.log("Error connecting database ... \n\n");  
+}
+});
+
+app.get("/",function(req,res){
+connection.query('SELECT * from Persons', function(err, rows, fields) {
+connection.end();
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+  });
+});
