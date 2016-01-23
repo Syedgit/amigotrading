98f324aa9c1a6f7e611a8ebb636d02a036f39584
syed hussain <syedhussain@syeds-MBP.home>
2016-01-23 16:18:52 -0500

    added Get api

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 'use strict';
-
 var express = require('express');
-var path = require('path');
-
 var app = express();
+var path = require('path');
+app.use(express.static('./'));
+
+var Obj = {firstName: "syed", lastName:"hussain"}
+
+app.get('/test', function (req, res) {
+  res.send(Obj);
+});
+
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static('./'));
-
-var server = app.listen(3000, function(){
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http',host,port);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
-var Firebase = require('firebase');
-var dataRef = new Firebase('hhttps://amigotradingcorp.firebaseio.com/');
-// dataRef.set({name: 'syed', text: 'hussain'});
+// var server = app.listen(3000, function(){
+// 	var host = server.address().address;
+// 	var port = server.address().port;
+// 	console.log('Example app listening at http',host,port);
+// });
 
-app.get('/wines', function(req, res) {
-    res.send([{name:'wine1'}, {name:'wine2'}]);
-});
+// app.get('/test', function(req, res) {
+//   res.type('text/plain'); // set content-type
+//   res.send('i am a beautiful butterfly'); // send text response
+// });
+
+// var Firebase = require('firebase');
+// var dataRef = new Firebase('hhttps://amigotradingcorp.firebaseio.com/');
+// // dataRef.set({name: 'syed', text: 'hussain'});
+
+// app.get('/wines', function(req, res) {
+//     res.send([{name:'wine1'}, {name:'wine2'}]);
+// });
 // var mysql      = require('mysql');
 // var connection = mysql.createConnection({
 //   host     : 'localhost',
--- scripts/workerController.js
+++ scripts/workerController.js
@@ -1,6 +1,14 @@
-angular.module('myApp').controller('WorkerController', function ($scope) {
+angular.module('myApp').controller('WorkerController', function ($scope,workerFactory) {
 	'use strict';
 	$scope.test = function(){
 		alert("first functiona is working");
 	};
+	$scope.data = {};
+	$scope.getTestData = function(){
+		workerFactory.getData().then(function(response){
+			$scope.data = response.data;
+			console.log("Data from server",$scope.data);
+		})
+	}
 });
+
